19ab688e1c487dc9f458af019ce6e80da0831288
Kostiantyn <kostya20022210@gmail.com>
2025-07-02 13:43:36 +0300

    Transition ContactForm to be functional component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,152 +1,140 @@
-import {Component} from 'react';
 import './ContactForm.css';
+import {useState} from 'react';
 
-export class ContactForm extends Component {
-    state = {
-        currentContact: {...this.props.currentContact},
-        EMPTY_CONTACT: {...this.props.EMPTY_CONTACT},
-    };
+function ContactForm({
+    currentContact,
+    EMPTY_CONTACT,
+    saveContact,
+    deleteContact,
+}) {
+    const [currentFormContact, setCurrentFormContact] = useState({
+        ...currentContact,
+    });
 
-    onInputChange = e => {
-        this.setState({
-            currentContact: {
-                ...this.state.currentContact,
-                [e.target.name]: e.target.value,
-            },
-        });
-    };
-
-    clearContactField = e => {
-        this.setState({
-            currentContact: {
-                ...this.state.currentContact,
-                [e.target.parentElement.querySelector('input').name]: '',
-            },
-        });
-    };
-
-    clearContact = () => {
-        this.setState({
-            currentContact: {
-                ...this.state.EMPTY_CONTACT,
-            },
-        });
-    };
-
-    static getDerivedStateFromProps(props, state) {
-        if (state.currentContact.id !== props.currentContact.id) {
-            return {currentContact: {...props.currentContact}};
-        } else {
-            return null;
-        }
+    if (currentFormContact.id !== currentContact.id) {
+        setCurrentFormContact({...currentContact});
     }
 
-    onFormSubmit = e => {
+    function onInputChange(e) {
+        setCurrentFormContact({
+            ...currentFormContact,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    function clearContactField(e) {
+        setCurrentFormContact({
+            ...currentFormContact,
+            [e.target.parentElement.querySelector('input').name]: '',
+        });
+    }
+
+    function clearContact() {
+        setCurrentFormContact({...EMPTY_CONTACT});
+    }
+
+    function onFormSubmit(e) {
         e.preventDefault();
-        this.props.saveContact(this.state.currentContact);
+        saveContact(currentFormContact);
 
-        if (!this.state.currentContact.id) {
-            this.clearContact();
+        if (!currentFormContact.id) {
+            clearContact();
         }
-    };
-
-    render() {
-        const {deleteContact} = this.props;
-
-        return (
-            <form onSubmit={this.onFormSubmit} className="contact-info">
-                <div className="form-info">
-                    <div className="input-wrapper">
-                        <input
-                            type="text"
-                            placeholder="First name"
-                            maxLength={25}
-                            name="firstName"
-                            onChange={this.onInputChange}
-                            value={this.state.currentContact.firstName}
-                            required
-                        />
-                        <button
-                            type="button"
-                            className="clear-info"
-                            onClick={this.clearContactField}
-                        >
-                            X
-                        </button>
-                    </div>
-                    <div className="input-wrapper">
-                        <input
-                            type="text"
-                            placeholder="Last name"
-                            maxLength={25}
-                            name="lastName"
-                            onChange={this.onInputChange}
-                            value={this.state.currentContact.lastName}
-                            required
-                        />
-                        <button
-                            type="button"
-                            className="clear-info"
-                            onClick={this.clearContactField}
-                        >
-                            X
-                        </button>
-                    </div>
-                    <div className="input-wrapper">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            onChange={this.onInputChange}
-                            value={this.state.currentContact.email}
-                        />
-                        <button
-                            type="button"
-                            className="clear-info"
-                            onClick={this.clearContactField}
-                        >
-                            X
-                        </button>
-                    </div>
-                    <div className="input-wrapper">
-                        <input
-                            type="tel"
-                            placeholder="Phone number"
-                            maxLength={20}
-                            name="phone"
-                            onChange={this.onInputChange}
-                            value={this.state.currentContact.phone}
-                        />
-                        <button
-                            type="button"
-                            className="clear-info"
-                            onClick={this.clearContactField}
-                        >
-                            X
-                        </button>
-                    </div>
-                </div>
-
-                <div className="btn-container form-submit">
-                    <button type="submit">Save</button>
-                </div>
-
-                {this.state.currentContact.id && (
-                    <div className="btn-container form-delete">
-                        <button
-                            type="button"
-                            // to make sure arguments are empty and event aren't passed through
-                            onClick={() => {
-                                deleteContact();
-                            }}
-                        >
-                            Delete
-                        </button>
-                    </div>
-                )}
-            </form>
-        );
     }
+
+    return (
+        <form onSubmit={onFormSubmit} className="contact-info">
+            <div className="form-info">
+                <div className="input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="First name"
+                        maxLength={25}
+                        name="firstName"
+                        onChange={onInputChange}
+                        value={currentFormContact.firstName}
+                        required
+                    />
+                    <button
+                        type="button"
+                        className="clear-info"
+                        onClick={clearContactField}
+                    >
+                        X
+                    </button>
+                </div>
+                <div className="input-wrapper">
+                    <input
+                        type="text"
+                        placeholder="Last name"
+                        maxLength={25}
+                        name="lastName"
+                        onChange={onInputChange}
+                        value={currentFormContact.lastName}
+                        required
+                    />
+                    <button
+                        type="button"
+                        className="clear-info"
+                        onClick={clearContactField}
+                    >
+                        X
+                    </button>
+                </div>
+                <div className="input-wrapper">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        onChange={onInputChange}
+                        value={currentFormContact.email}
+                    />
+                    <button
+                        type="button"
+                        className="clear-info"
+                        onClick={clearContactField}
+                    >
+                        X
+                    </button>
+                </div>
+                <div className="input-wrapper">
+                    <input
+                        type="tel"
+                        placeholder="Phone number"
+                        maxLength={20}
+                        name="phone"
+                        onChange={onInputChange}
+                        value={currentFormContact.phone}
+                    />
+                    <button
+                        type="button"
+                        className="clear-info"
+                        onClick={clearContactField}
+                    >
+                        X
+                    </button>
+                </div>
+            </div>
+
+            <div className="btn-container form-submit">
+                <button type="submit">Save</button>
+            </div>
+
+            {currentContact.id && (
+                <div className="btn-container form-delete">
+                    <button
+                        type="button"
+                        // to make sure arguments are empty and event aren't passed through
+                        onClick={() => {
+                            deleteContact();
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            )}
+        </form>
+    );
 }
 
 export default ContactForm;
