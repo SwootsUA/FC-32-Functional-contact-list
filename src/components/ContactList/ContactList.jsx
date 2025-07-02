8e101b9ac0a2b0da5d1ae8ea0b60000593bae325
Kostiantyn <kostya20022210@gmail.com>
2025-07-02 13:42:49 +0300

    Transition ListItem and ContactList to be functional components

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,23 @@
-import {Component} from 'react';
 import ListItem from '../ListItem/ListItem';
 import './ContactList.css';
 
-export class ContactList extends Component {
-    render() {
-        const {contacts, enterEditMode, deleteContact} = this.props;
+import React from 'react';
 
-        return (
-            <div className="scroll-box">
-                <ul>
-                    {contacts.map(contact => (
-                        <ListItem
-                            contact={contact}
-                            enterEditMode={enterEditMode}
-                            key={contact.id}
-                            deleteContact={deleteContact}
-                        />
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+function ContactList({contacts, enterEditMode, deleteContact}) {
+    return (
+        <div className="scroll-box">
+            <ul>
+                {contacts.map(contact => (
+                    <ListItem
+                        contact={contact}
+                        enterEditMode={enterEditMode}
+                        key={contact.id}
+                        deleteContact={deleteContact}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ContactList;
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,25 +1,20 @@
-import {Component} from 'react';
 import './ListItem.css';
 
-export class ListItem extends Component {
-    render() {
-        const {contact, enterEditMode, deleteContact} = this.props;
-
-        return (
-            <li onDoubleClick={() => enterEditMode(contact.id)}>
-                {`${contact.firstName} ${contact.lastName}`}
-                <button
-                    className="delete-user-btn"
-                    onClick={e => {
-                        e.stopPropagation();
-                        deleteContact(contact.id);
-                    }}
-                >
-                    X
-                </button>
-            </li>
-        );
-    }
+function ListItem({contact, enterEditMode, deleteContact}) {
+    return (
+        <li onDoubleClick={() => enterEditMode(contact.id)}>
+            {`${contact.firstName} ${contact.lastName}`}
+            <button
+                className="delete-user-btn"
+                onClick={e => {
+                    e.stopPropagation();
+                    deleteContact(contact.id);
+                }}
+            >
+                X
+            </button>
+        </li>
+    );
 }
 
 export default ListItem;
